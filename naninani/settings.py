4f75aba88909ae41d288603f0c1897564367ad14
Gustav Larsson <gustav.m.larsson@gmail.com>
2019-11-10 14:02:27 -0800

    Remove localhost from settings.py (it's in debug.py)

--- naninani/settings.py
+++ naninani/settings.py
@@ -29,11 +29,6 @@ ALLOWED_HOSTS = [
     'glarsson.pythonanywhere.com',
 ]
 
-if DEBUG:
-    ALLOWED_HOSTS.append('localhost')
-    ALLOWED_HOSTS.append('127.0.0.1')
-
-
 # Application definition
 
 INSTALLED_APPS = [
